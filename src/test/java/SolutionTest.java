5af996d93b6ac48b81dcc03fcdef35ce4ea259d4
alek <wait82985@gmail.com>
2023-08-03 11:29:12 +0600

    initial commit. 3 test cases

---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,36 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolutionTest {
+    @Test
+    public void letterCombinationsTest1(){
+        String digits = "23";
+        List<String> expected = new ArrayList<>(List.of("ad","ae","af","bd","be","bf","cd","ce","cf"));
+        List<String> actual = new Solution().letterCombinations(digits);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void letterCombinationsTest2(){
+        String digits = "";
+        List<String> expected = new ArrayList<>(List.of());
+        List<String> actual = new Solution().letterCombinations(digits);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void letterCombinationsTest3(){
+        String digits = "2";
+        List<String> expected = new ArrayList<>(List.of("a","b","c","bd"));
+        List<String> actual = new Solution().letterCombinations(digits);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+}
